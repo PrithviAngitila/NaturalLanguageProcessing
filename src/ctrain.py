07f28e81e303e3e19ac13dc7f27c48805886989c
PrithviAngitila <31962225+PrithviAngitila@users.noreply.github.com>
2018-03-02 04:58:09 +0000

    Create ctrain.py

---
+++ src/ctrain.py
@@ -0,0 +1,21 @@
+self NNP B-NP
+driving VBG I-NP
+cars NNS B-VH
+is VBZ B-NP
+an DT B-NP
+application NN B-NP
+of IN I-NP
+Deep NNP B-Tech
+Learning NNP I-Tech
+, , O
+
+self NNP B-NP
+driving VBG I-NP
+trucks NNS B-VH
+is VBZ B-NP
+an DT B-NP
+application NN B-NP
+of IN I-NP
+Deep NNP B-Tech
+Learning NNP I-Tech
+, , O
